6f95b470de4ff133051db95f087b06f2e7cef108
riru <riru4293@gmail.com>
2026-01-04 23:01:23 +0900

    trial45

--- components/machine/property/include/value.hpp
+++ components/machine/property/include/value.hpp
@@ -8,4 +8,7 @@ namespace machine::property
 {
     /** @copydoc ::value::Value255 */
     using Value = ::value::Value255;
+
+    /** @copydoc ::value::MutableValue255 */
+    using MutableValue = ::value::MutableValue255;
 }
